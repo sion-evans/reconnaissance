796e7b86a653e0a9f6c2a6dd9693dfc8395841f6
Sion Evans <59143478+sion-evans@users.noreply.github.com>
2020-01-20 10:32:46 +0000

    Update zone-transfer.sh

--- zone-transfer.sh
+++ zone-transfer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-domain=$1
+if [ -z "$1" ]
+  then
+    echo "Usage: $0 <domain>"
+	exit 1
+fi
 
-host -t ns $domain | cut -f 4 -d ' ' | sed -e 's/\.$//' | while read -r line ; do
-    host -l $domain $line
+host -t ns $1 | cut -f 4 -d ' ' | sed -e 's/\.$//' | while read -r line ; do
+    host -l $1 $line
 done
